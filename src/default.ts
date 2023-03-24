7be93f35a98930dc5f000704118f37fbcf833279
Gavin Vincent <34804116+Gavincent1@users.noreply.github.com>
2023-03-25 01:18:05 +0800

    Update default.ts
    更改提示信息

--- src/default.ts
+++ src/default.ts
@@ -10,8 +10,6 @@ export const defaultMessage = `
 
 - 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 
-- 本站由 Hell0S 搭建。
-
-- 想要部署的朋友，可关注公众号：**Hell0S**，添加我个人微信获取保姆级部署文档，
+- 本站由 Hell0S 搭建，可关注公众号：**Hell0S** 。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
